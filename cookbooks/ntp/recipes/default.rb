bf89b61307cf36800d4ed87097f4b01f1644c167
captainshar <captainshar@gmail.com>
2015-02-19 23:57:20 +0000

    Setting it up on GitHub

--- cookbooks/ntp/recipes/default.rb
+++ cookbooks/ntp/recipes/default.rb
@@ -2,4 +2,20 @@
 # Cookbook Name:: ntp
 # Recipe:: default
 #
-# Copyright (c) 2015 The Authors, All Rights Reserved.
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+#
+package 'ntp' do
+  action :install
+end
+
+package 'ntpdate' do
+  action :install
+end
+
+template "/etc/ntp.conf" do
+  action :create
+  source "ntp.erb"
+  mode "0644"
+  owner "root"
+  group "root"
+end
